bc15be38266eac2e6f943d0797239b898690b6a3
qpongratz <lagarathan@gmail.com>
2021-09-14 13:48:58 -0500

    Associate post and user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true, length: { in: 6..20 }
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { in: 8..20 }
+  has_many :posts
 end
